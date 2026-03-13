ca773753d85ddf40723093c4f675a266d6aeec3f
TakeeRyu <TakeeRyu@DESKTOP-CJSQJGP>
2026-03-13 22:48:00 +0800

    添加项目文件。

---
+++ SlayTheSpire2ModManager.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SlayTheSpire2ModManager.App.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ SlayTheSpire2ModManager.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SlayTheSpire2ModManager.App.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
